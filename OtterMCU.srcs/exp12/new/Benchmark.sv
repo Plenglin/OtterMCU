77b4614ec0e7983a6aa833d0bd42c8aab7634faa
Astrid Yu <astrid@astrid.tech>
2021-01-26 21:32:27 -0800

    Benchmarks of stuff

--- OtterMCU.srcs/exp12/new/Benchmark.sv
+++ OtterMCU.srcs/exp12/new/Benchmark.sv
@@ -1,24 +1,4 @@
 `timescale 10ns / 1ns
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01/24/2021 12:03:44 PM
-// Design Name: 
-// Module Name: Benchmark
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Benchmark();
     reg RST; 
@@ -29,7 +9,7 @@ module Benchmark();
     wire [31:0] iobus_out; 
     wire iobus_wr; 
 
-    OTTER_MCU #(.MEM_FILE("test_all.mem"))  my_otter(
+    OTTER_MCU #(.MEM_FILE("bench.mem"))  my_otter(
      .RST         (RST),
      .intr        (intr),
      .clk         (clk),
@@ -57,9 +37,9 @@ module Benchmark();
         RST = 0;
         start = $time;
         $display("Started", start);
-        forever #1 if (my_otter.prog_counter.addr === 32'h1ec) begin
-            $display("Reached %d in %l", my_otter.prog_counter.addr, $time - start); 
-            $stop;
+        forever #1 if (my_otter.prog_counter.addr === 32'h1e8) begin
+             $display("Reached %d in %l", my_otter.prog_counter.addr, $time - start); 
+             $stop;
         end
     end
 endmodule
