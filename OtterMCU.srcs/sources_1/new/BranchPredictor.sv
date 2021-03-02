807e1c5149f76d5b18797103ffce4e6a6bf91397
Astrid Yu <astrid@astrid.tech>
2021-03-02 11:01:43 -0800

    Add modports to the predictor

--- OtterMCU.srcs/sources_1/new/BranchPredictor.sv
+++ OtterMCU.srcs/sources_1/new/BranchPredictor.sv
@@ -1,9 +1,41 @@
+import Types::*;
+
 interface BranchPredictor(
     input clk,
-    input reset,
-    input [31:0] pc,
-    input is_branch,
-    input func3_t branch_type,
-    output should_branch
+    input reset
 );
+    logic id_is_branch;
+    func3_t id_branch_type;
+    logic [31:0] id_pc;
+    
+    logic ex_branched;
+    func3_t ex_branch_type;
+    logic [31:0] ex_pc;
+    
+    logic should_branch;
+
+    modport Predictor(
+        input id_is_branch,
+        input id_branch_type,
+        input id_pc,
+        
+        input ex_branched,
+        input ex_branch_type,
+        input ex_pc,
+        
+        output should_branch
+    );
+    
+    modport ID(
+        output id_is_branch,
+        output id_branch_type,
+        output id_pc,
+        input should_branch
+    );
+    
+    modport EX(
+        output ex_branched,
+        output ex_branch_type,
+        output ex_pc
+    );
 endinterface
